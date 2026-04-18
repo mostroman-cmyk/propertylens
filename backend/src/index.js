972ca0cb9a0dfdb469f7972ba6d4e38c8673f403
Matt Ostroman <mostroman@yahoo.com>
2026-04-18 11:43:32 -0700

    serve frontend from backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const migrate = require('./db/migrate');
 
 const propertiesRouter   = require('./routes/properties');
@@ -13,6 +14,8 @@ const settingsRouter     = require('./routes/settings');
 const app  = express();
 const PORT = process.env.PORT || 3001;
 
+const FRONTEND_DIST = path.join(__dirname, '..', '..', 'frontend', 'dist');
+
 app.use(cors());
 app.use(express.json());
 
@@ -24,6 +27,11 @@ app.use('/api/transactions', transactionsRouter);
 app.use('/api/plaid',        plaidRouter);
 app.use('/api/email',        emailRouter);
 app.use('/api/settings',     settingsRouter);
+
+app.use(express.static(FRONTEND_DIST));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(FRONTEND_DIST, 'index.html'));
+});
 
 async function start() {
   app.listen(PORT, () => {
